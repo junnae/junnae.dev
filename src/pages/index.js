ed7b586e339d37fb4f70c93f01eeb26c3604cf05
Aleksander Andresen <caeses@gmail.com>
2021-10-13 21:16:03 +0200

    rename files for deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import Header from "../components/Header";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import MainContent from "../components/MainContent";
 
 const pageStyles = {
